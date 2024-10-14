7a4f22fdabaa9d3e16109f1f69301ead9264eb46
tacker530 <tacker530i@gmail.com>
2024-10-14 18:33:01 +0900

    json整理した

--- 1.draw/makecsv.bat
+++ 1.draw/makecsv.bat
@@ -5,3 +5,6 @@ cd %BASEDIR%\1.draw
 call %BASEDIR%\tools\make.bat aomi-face.json  > %BASEDIR%\2.portals\aomi-face.csv
 call %BASEDIR%\tools\make.bat aomi-ramen.json > %BASEDIR%\2.portals\aomi-ramen.csv
 call %BASEDIR%\tools\make.bat aomi-ra.json    > %BASEDIR%\2.portals\aomi-ra.csv
+
+call %BASEDIR%\tools\make.bat aomi-all.json    > %BASEDIR%\2.portals\aomi-all.csv
+
--- 1.draw/makekml.bat
+++ 1.draw/makekml.bat
@@ -5,3 +5,5 @@ cd %BASEDIR%\1.draw
 call %BASEDIR%\tools\kml.bat aomi-face.json  > %BASEDIR%\3.kml\aomi-face.kml
 call %BASEDIR%\tools\kml.bat aomi-ramen.json > %BASEDIR%\3.kml\aomi-ramen.kml
 call %BASEDIR%\tools\kml.bat aomi-ra.json    > %BASEDIR%\3.kml\aomi-ra.kml
+
+call %BASEDIR%\tools\kml.bat aomi-all.json    > %BASEDIR%\3.kml\aomi-all.kml
